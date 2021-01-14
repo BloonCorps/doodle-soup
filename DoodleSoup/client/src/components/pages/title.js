ee76f814c49e55d323ebf400506f00e3b50004c9
Jared Zheng <jared@Jared.mit.edu>
2021-01-13 22:45:04 -0500

    fixed login

--- DoodleSoup/client/src/components/pages/title.js
+++ DoodleSoup/client/src/components/pages/title.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "../../utilities.css";
 import "./title.css"
+
+
+//This below is the correct client ID, Stuff for making login button
 import GoogleLogin, { GoogleLogout } from "react-google-login";
-
-//This below is the correct client ID
 const GOOGLE_CLIENT_ID = "666170878713-705kglliuqe3jr4l0mha34ei8d862qud.apps.googleusercontent.com";
-
 
 class Title extends Component {
     constructor(props) {
@@ -17,7 +17,6 @@ class Title extends Component {
     componentDidMount() {
         // remember -- api calls go here!
       }
-
 
     render () {
         return (
