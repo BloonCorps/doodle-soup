85cca3217cbab60e6c5badb4b9466100ae0e4ea3
Vincent Chen <vtchen@mit.edu>
2021-01-24 13:28:03 -0800

    better post bars

--- DoodleSoup/client/src/components/modules/Post.js
+++ DoodleSoup/client/src/components/modules/Post.js
@@ -6,6 +6,8 @@ import { post, get } from "../../utilities";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 const GOOGLE_CLIENT_ID = "666170878713-705kglliuqe3jr4l0mha34ei8d862qud.apps.googleusercontent.com";
 
+let tagContent = "";
+
 class Post extends Component {
   constructor(props) {
     super(props);
@@ -20,6 +22,15 @@ class Post extends Component {
 }
 
   render() {
+    console.log(this.props.tags)
+    tagContent = ""
+    this.props.tags.forEach(tagName => tagContent = tagContent + " " + tagName + " |")
+    if (tagContent === "") {
+      tagContent = "No tags"
+    } else {
+      tagContent = "|" + tagContent;
+    }
+
     if (this.props.creator_id === this.props.userId) {
       return (
         //{this.props.creator_id}
@@ -45,7 +56,7 @@ class Post extends Component {
           <img className="imgWorkFeed" src={this.props.pic}/>
 
           <div className="taggedPost"> 
-          <p className="tagContent"> {this.props.tags} </p> 
+          <p className="tagContent"> {tagContent} </p> 
           </div>
           
         </div>)
@@ -64,7 +75,7 @@ class Post extends Component {
 
           <div className="taggedPost"> 
           <p className="tagContent"> 
-          {this.props.tags} 
+          {tagContent} 
           </p> 
           </div>
 
